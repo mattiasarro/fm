9ea27bae0df3a0ce8f9e301bbae09282dd89683d
mattiasarro <mattias.arro@gmail.com>
2018-12-28 19:12:15 +0000

    predict

---
+++ bert/predict.sh
@@ -0,0 +1,18 @@
+export BERT_BASE_DIR=data/uncased_L-12_H-768_A-12
+export GLUE_DIR=data/glue_data
+export SEME_DIR=data/stance_detection
+
+python bert/run_classifier.py \
+    --task_name=seme \
+    --do_train=false \
+    --do_eval=false \
+    --do_predict=true \
+    --data_dir=$SEME_DIR \
+    --vocab_file=$BERT_BASE_DIR/vocab.txt \
+    --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+    --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+    --max_seq_length=128 \
+    --train_batch_size=32 \
+    --learning_rate=2e-5 \
+    --num_train_epochs=3.0 \
+    --output_dir=predictions
