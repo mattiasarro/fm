eecb1fc3868d44073bb098d148d9e37475c00503
mattiasarro <mattias.arro@gmail.com>
2018-12-30 19:37:55 +0000

    change hparams

--- bin/run.sh
+++ bin/run.sh
@@ -9,7 +9,7 @@ $PYTHON bert/custom_run.py \
     --bert_config_file=$BERT_BASE_DIR/bert_config.json \
     --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
     --max_seq_length=128 \
-    --train_batch_size=32 \
-    --learning_rate=2e-5 \
-    --num_train_epochs=5.0 \
-    --output_dir=/tmp/seme_output/
+    --train_batch_size=16 \
+    --learning_rate=3e-5 \
+    --num_train_epochs=15.0 \
+    --output_dir=seme
